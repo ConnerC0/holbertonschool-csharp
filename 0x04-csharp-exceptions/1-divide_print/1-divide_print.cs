c5a3dc3b038a77a3cbaf5409b15a62dfa4b0ddf1
ConnerC0 <connercurtis72@gmail.com>
2023-01-18 17:20:07 -0600

    fix

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 
-namespace _1_divide_print
+class Int
 {
-    class Int
+    public static void divide(int a, int b)
     {
-        public static void divide(int a, int b)
+        int c = 0;
+        try
         {
-            int c = 0;
-            try
-            {
-                c = a / b;
-            }
-            catch
-            {
-                Console.WriteLine("Cannot divide by zero");
-            }
-            finally
-            {
-                Console.WriteLine("{0} / {1} = {2}", a, b, c);
-            }
+            c = a / b;
+        }
+        catch
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+             Console.WriteLine("{0} / {1} = {2}", a, b, c);
         }
     }
 }
