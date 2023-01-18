c1ef66894d133fc565ae7e29363da9afefd05c01
ConnerC0 <connercurtis72@gmail.com>
2023-01-18 16:56:19 -0600

    task 0 + files initiated for other tasks

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _0_safe_list_print
+{
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+        int i = 0;
+        try
+        {
+            for (; i < n; i++)
+                Console.WriteLine(myList[i]);
+        }
+        catch (Exception)
+        {
+            return (i);
+        }
+        return (i);  
+        }
+    }
+}
