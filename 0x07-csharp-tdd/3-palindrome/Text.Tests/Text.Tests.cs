3456773cd2007348e5ef91a0ab09e23e11808380
ConnerC0 <connercurtis72@gmail.com>
2023-01-25 22:34:01 -0600

    corrections

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -26,7 +26,7 @@ namespace Text.Tests
 		public void Test5()
 		{
 			string str = "";
-            Assert.That(Text.Str.IsPalindrome(test), Is.False);
+            Assert.That(Text.Str.IsPalindrome(test), Is.True);
 		}
     }
 }
